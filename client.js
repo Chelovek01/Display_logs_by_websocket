3acb0bd541eeebc6ba95bb5bd6354012bc4d5be9
BaranBayan <85448845+BaranBayan@users.noreply.github.com>
2022-06-03 08:43:26 +0300

    Микрофиксы

--- client.js
+++ client.js
@@ -8,16 +8,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
     buttonOpen.onclick = function () {
 
+        textContainer.innerHTML = ''
+        buttonOpen.disabled = true
+        buttonOpen.style.background = '#8b8b8b'
+
         let webSocket = new WebSocket('ws://localhost:12345/')
 
         webSocket.onopen = function () {
             webSocket.send('is_open')
+            textContainer.innerHTML = ''
+            buttonOpen.disabled = true
+            buttonOpen.style.background = '#8b8b8b'
+            alert('Connection is open')
         }
-        alert('Connection is open')
+
 
         webSocket.onmessage = function (event) {
 
             let text = event.data.split('\n')
+
             for (let i = 0; i < text.length; i++) {
                 if (text[i].length !== 0) {
                     let li = document.createElement("li")
@@ -27,26 +36,24 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
 
-
         buttonClose.onclick = function () {
 
+            buttonOpen.disabled = false
+            buttonOpen.style.background = '#2fb380'
+
+
             if (webSocket.readyState === 1) {
-                webSocket.close(1000, "работа закончена");
+                webSocket.close(1000, "Work ended");
                 alert('Connection closed')
             } else {
-                alert('соединеие не было открыто')
+                alert('The connection was not opened')
             }
         }
     }
 
+    buttonClose.onclick = function () {
+
+        alert('The connection was not opened')
+    }
 
 }, false)
-
-
-// function startSocket() {
-//     var ws = new WebSocket("ws://localhost:8765/")
-//     ws.onopen = function(event) {
-//         ws.send("Sent this from client.js")
-//     }
-// }
-// startSocket();
--- server.py
+++ server.py
@@ -3,32 +3,36 @@ import os
 import websockets
 
 
-async def time(websocket, path):
-
+async def send_logs(websocket, path):
     print('New user, connected')
 
     filename = r"C:\Users\User\Downloads\Telegram Desktop\debug_scoring.supervisor.log"
     date = 0
-    f = open(filename, "r")
-    result = f.readlines()[-4:]
+    f = open(filename, "r", encoding="utf-8")
+    result = f.readlines()[-100:]
     position = f.tell()
+    f.close()
 
     while True:
         try:
+
             date_1 = os.path.getmtime(filename)
 
             try:
-                new_message = await asyncio.wait_for(websocket.recv(), timeout=2)
+                new_message = await asyncio.wait_for(websocket.recv(), timeout=1)
                 await websocket.send(result)
                 print(new_message)
             except asyncio.TimeoutError:
                 print('timeout!')
+                pass
 
             if date_1 != date:
                 date = date_1
+                f = open(filename, "r", encoding="utf-8")
                 f.seek(position)
                 result2 = f.read()
                 position = f.tell()
+                f.close()
                 await websocket.send(result2)
                 await asyncio.sleep(1)
 
@@ -38,7 +42,7 @@ async def time(websocket, path):
 
 
 async def start_serv():
-    await websockets.serve(time, 'localhost', 12345)
+    await websockets.serve(send_logs, 'localhost', 12345)
 
 
 if __name__ == '__main__':
